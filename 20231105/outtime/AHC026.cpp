a68d1bf8b85f7701dde8944dbaf4452081e2aceb
kawabata <ky381011gitpgmail.com>
2023-11-05 22:11:02 +0900

    AHC026TLE

--- 20231105/outtime/AHC026.cpp
+++ 20231105/outtime/AHC026.cpp
@@ -6,52 +6,25 @@ int main(){
     int n, m;
     cin >> n >> m;
     int l = n/m;
-    vector<vector<int>> b(n, vector<int>(l));
+    vector<vector<int>> b(m, vector<int>(l));
     for(int i=0; i<m; i++){
         for(int j=0; j<l; j++){
             cin >> b[i][j];
         }
     }
-
-    vector<vector<int>> c(n, vector<int>(l));
-    for(int i=0; i<m; i++){
-        for(int j=0; j<l; j++){
-            c[i][j] = b[i][j];
-        }
-    }
-
-    for(int i=0; i<8; i++){
-        int temp = l - 1;
-        for(int j=0; j<20; j++){
-            cout << (*max_element(c[i].begin(), c[i].end())) << " 9" << endl;
-            cout << c[i][temp] << " 10" << endl;
-            cout << (*max_element(c[i].begin(), c[i].end())) << " " << i+1 << endl;
-            cout << c[i][temp] << " " << i+1 << endl;
-            c[i].erase(max_element(c[i].begin(), c[i].end()));
-            temp -= 1;
-        }
-    }
-    for(int i=8; i<10; i++){
-        int temp = l - 1;
-        for(int j=0; j<20; j++){
-            cout << (*max_element(c[i].begin(), c[i].end())) << " 1" << endl;
-            cout << c[i][temp] << " 2" << endl;
-            cout << (*max_element(c[i].begin(), c[i].end())) << " " << i+1 << endl;
-            cout << c[i][temp] << " " << i+1 << endl;
-            c[i].erase(max_element(c[i].begin(), c[i].end()));
-            temp -= 1;
-        }
-    }
-
-    for(int i=0; i<m; i++){
-        sort(b[i].begin(), b[i].end());
-    }
-
-    for(int i=1; i<=200; i++){
+    for(int i=1; i<=n; i++){
         for(int j=0; j<m; j++){
-            for(int k=0; k<l; k++){
-                if(b[j][k] == i)
-                    cout << j+1 <<" 0" << endl;
+            for(int k=0; k<b[j].size(); k++){
+                if(b[j][k] == i){               
+                     for(int l=1; l<=b[j].size()-k-1; l++){
+                        b[9-j].push_back(b[j][k+l]);
+                     }
+                     for(int l=1; l<=b[j].size()-k-1; l++){
+                        b[j].erase(b[j].end()-1);
+                     }
+                     cout << i << " " << 9-j << endl;
+                     cout << i << " 0" << endl;
+                }
             }
         }
     }
