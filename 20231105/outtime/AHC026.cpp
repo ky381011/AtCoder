93acd1371016ff7243d2f85e0fee76fe34d75914
kawabata <ky381011gitpgmail.com>
2023-11-08 02:00:45 +0900

    うまくはいってない

--- 20231105/outtime/AHC026.cpp
+++ 20231105/outtime/AHC026.cpp
@@ -14,16 +14,18 @@ int main(){
     }
     for(int i=1; i<=n; i++){
         for(int j=0; j<m; j++){
-            for(int k=0; k<b[j].size(); k++){
-                if(b[j][k] == i){               
-                     for(int l=1; l<=b[j].size()-k-1; l++){
+            int size = b[j].size();
+            for(int k=0; k<size; k++){
+                if(b[j][k] == i ){ 
+                    for(int l=1; l<=size-k-1; l++){
                         b[9-j].push_back(b[j][k+l]);
-                     }
-                     for(int l=1; l<=b[j].size()-k-1; l++){
+                    }
+                    for(int l=1; l<=b[j].size()-k; l++){
                         b[j].erase(b[j].end()-1);
-                     }
-                     cout << i << " " << 9-j << endl;
-                     cout << i << " 0" << endl;
+                    }
+                    cout << i << " " << 9-j << endl;
+                    cout << i << " 0" << endl;
+                    break;
                 }
             }
         }
