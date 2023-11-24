47ceba926e7fbe04945eb581738d04db026957b3
kawabata <ky381011gitpgmail.com>
2023-11-25 04:17:55 +0900

    TLE未解決

--- past/320ABC/320d.cpp
+++ past/320ABC/320d.cpp
@@ -9,42 +9,34 @@ int main(){
     for(int i=0; i<M; i++){
         cin >> A[i] >> B[i] >> X[i] >> Y[i];
     }
-    vector<long> OX(N), OY(N), STATE(N);
+    vector<long long> OX(N), OY(N);
+    vector<int> STATE(N);
     OX[0] = 0;
     OY[0] = 0;
     STATE[0] = 1;
 
     int left = M;
     int count = 1;
-    while(count > 0 && left > 0){
+    int decided = 1;
+    while(count > 0 && left > 0 && decided < N){
         count = 0;
-        for(int i=0; i<left; i++){
-            if(STATE[A[i] - 1] == 1){
+        for(int i=0; i<M; i++){
+            if(STATE[A[i] - 1] == 1 && STATE[B[i] - 1] != 1){
                 OX[B[i] - 1] = OX[A[i] - 1] + X[i];
                 OY[B[i] - 1] = OY[A[i] - 1] + Y[i];
                 STATE[B[i] - 1] = 1;
-                A.erase(A.begin() + i);
-                B.erase(B.begin() + i);
-                X.erase(X.begin() + i);
-                Y.erase(Y.begin() + i);
                 count++;
-                left--;
-                break;
-            } else if(STATE[B[i] - 1] == 1){
+                decided++;
+            } else if(STATE[B[i] - 1] == 1 && STATE[A[i] - 1] != 1){
                 OX[A[i] - 1] = OX[B[i] - 1] - X[i];
                 OY[A[i] - 1] = OY[B[i] - 1] - Y[i];
                 STATE[A[i] - 1] = 1;
-                A.erase(A.begin() + i);
-                B.erase(B.begin() + i);
-                X.erase(X.begin() + i);
-                Y.erase(Y.begin() + i);
                 count++;
-                left--;
-                break;
+                decided++;
             }
         }
+        left -= count;
     }
-
     for(int i=0; i<N; i++){
         if(STATE[i] == 1){
             cout << OX[i] << " " << OY[i] << endl;
